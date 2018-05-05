6d6f22031048774a02e54a19afd6455c62feb924
James Ferguson <ncjamesferguson@gmail.com>
2018-05-05 05:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-
+  return drivers.slice(0, drivers.length - 1)
 }
 
 function removeFirstDriver() {
-
+  return drivers.slice(1)
 }
